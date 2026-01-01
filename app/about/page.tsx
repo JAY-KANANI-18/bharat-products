7842ae8267f8ff640c65cc13edb9cd112e5ac2de
Jay-kanani18 <jkanani.elluminati@gmail.com>
2026-01-01 15:39:23 +0530

    update grid in about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -894,7 +894,7 @@ export default function AboutPage() {
             </div>
           </AnimatedSection>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-20">
             {[
               {
                 icon: Users,
